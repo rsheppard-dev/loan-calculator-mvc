1a8e61534402644f043c324df52bf58b69fb9c05
Roy Sheppard <rsheppard83@gmail.com>
2023-02-02 20:25:04 +0000

    Loan Calculator remade in C#.
    Loan Calculator remade in C#.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace loan_calculator_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
